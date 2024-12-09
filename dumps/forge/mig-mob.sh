3fe70030d63cad6b96a23dd6ada7f72d5a5e5414
moeng <fbsd61@gmail.com>
2024-12-09 14:24:14 +0200

    BPP/29/d242dadc95
    + Some typo fixes by IVBela + Fixed Bio Lab spawns to official jRO +
      Added some Kiel building warps thanks to Celestria + Fixed a few warps
    warping you back (they've been there for long =X) + Corrected some
    errors in cities/hugel.txt and guides/guides_hu.txt // + Некоторые
    исправления опечаток от IVBela + Исправлены спавны Bio Lab в официальном
    jRO + Добавлены некоторые килевые строительные варпы благодаря Celestria
    + Исправлены несколько варпов, которые вернули вас обратно (они были там
    долгое время =X) + Исправлены некоторые ошибки в cities/hugel.txt и
    guides/guides_hu.txt + Added cast time to pick stone of 3 secs and 500ms
    delay + Fixed produce_db for Halberd 3-->12 Oridecons + Fixed Violy's
    Violin[4] drop rate. It is 2% + Fixed a typo and sorted skill_cast_db by
    skill id  // + Добавлено время каста для подбора камня в 3 секунды и
    задержка в 500 мс + Исправлен produce_db для Halberd 3-->12 Oridecons +
    Исправлена вероятность выпадения Violy's Violin[4]. Она составляет 2% +
    Исправлена опечатка и отсортирован skill_cast_db по идентификатору
    навыка

---
+++ dumps/forge/mig-mob.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "" >./end.sql
+
+H="ID,Name,Name2,LV,HP,SP,EXP,JEXP,Range1,ATK1,ATK2,DEF,MDEF,STR,AGI,VIT,INT,DEX,LUK,Range2,Range3,Scale,Race,Element,Mode,Speed,ADelay,aMotion,dMotion,Drop1id,Drop1per,Drop2id,Drop2per,Drop3id,Drop3per,Drop4id,Drop4per,Drop5id,Drop5per,Drop6id,Drop6per,Drop7id,Drop7per,Drop8id,Drop8per,Drop9id,Drop9per,DropCardid,DropCardper,MEXP,ExpPer,MVP1id,MVP1per,MVP2id,MVP2per,MVP3id,MVP3per"
+IFS="," read -r -a array <<< "$H"
+c=0
+for x in "${array[@]}"
+ do
+  a[$c]="$x"
+  ((c++))
+ done
+
+
+DEF=$IFS
+IFS=$'\n'
+c=0
+for x in `grep "^\+" ./src.sql|sed -e 's/^\+//'`;
+  do
+    STRb=""
+    c=0
+    IFS="," read -r -a array <<< "${x}"
+    for y in "${array[@]}"
+      do
+        if [ "$c" == "57" ]
+          then
+            break
+          fi
+	STRb+=" ${a[$c]} = $y,"
+#	echo "$c	${a[$c]}	$y"
+	((c++))
+    done
+
+    STRa="update mob_db set "
+    STRc=" where id = \"${array[0]}\""
+    echo -e "$STRa${STRb:0:-1}$STRc" >> ./end.sql
+
+
+done
+
+#ID,Name,JName,LV,HP,SP,EXP,JEXP,Range1,ATK1,ATK2,DEF,MDEF,STR,AGI,VIT,INT,DEX,LUK,Range2,Range3,Scale,Race,Element,Mode,Speed,ADelay,aMotion,dMotion,Drop1id,Drop1per,Drop2id,Drop2per,Drop3id,Drop3per,Drop4id,Drop4per,Drop5id,Drop5per,Drop6id,Drop6per,Drop7id,Drop7per,Drop8id,Drop8per,Drop9id,Drop9per,DropCardid,DropCardper,MEXP,ExpPer,MVP1id,MVP1per,MVP2id,MVP2per,MVP3id,MVP3per
+
+
+
+
+
+
