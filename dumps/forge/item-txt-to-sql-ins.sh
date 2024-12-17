5c452381036af20c781e824cfcf7d229e2a0d8d2
moeng <fbsd61@gmail.com>
2024-12-17 10:24:39 +0200

    FIX/41/sync items from txt to sql

---
+++ dumps/forge/item-txt-to-sql-ins.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DEF=$IFS
+rm -f ./end.sql
+
+STRa="insert ignore into \`item_db\` VALUES ("
+
+H="id,name_english,name_japanese,type,price_buy,price_sell,weight,attack,defence,range,slots,equip_jobs,equip_upper,equip_genders,equip_locations,weapon_level,equip_level,refineable,view,script"
+IFS="," read -r -a array <<< "$H"
+c=0
+for x in "${array[@]}"
+ do
+  a[$c]="$x"
+  ((c++))
+ done
+
+IFS=$'\n'
+C=0
+for x in `grep "^[0-9]" ../../db/item_db.txt|tr -d '}'`;
+ do
+#  b[$C]=$x
+  STRb=""
+  IFS="{"
+  A=0
+  for y in `echo "$x"`;
+   do
+    if [ "0" == "$A" ]
+     then
+#      c[$C]=$y
+      B=0
+      IFS="," read -r -a array <<< "${y}"
+      for z in "${array[@]}";
+       do
+        if [ "$B" == "21" ]
+         then
+          break
+        fi
+        if [ -n "$z" ]
+         then
+          #z[$B]="$z"
+          STRb+=" \"$z\","
+         else
+          #z[$B]="$z"
+          STRb+=" NULL,"
+        fi
+        ((B++))
+      done
+      ((A++))
+     else
+      if [ -n "$z" ]
+       then
+        STRb+=" '$y'"
+       else
+        STRb+=" NULL"
+      fi
+      A=0
+    fi
+  done
+  STRc=" )"
+  echo -e "${STRa}${STRb}${STRc};" >> ./end.sql
+  ((C++))  
+done
---
+++ dumps/migrations/8-fix-item_db-modify-names.sql
@@ -0,0 +1,2 @@
+alter table item_db modify name_japanese varchar(50);
+alter table item_db modify name_english varchar(50);
