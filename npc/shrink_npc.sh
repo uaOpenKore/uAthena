4f9e4df7d28eb0f8f9fdd34eab8a178661803887
moeng <fbsd61@gmail.com>
2025-01-04 20:57:38 +0200

    FIX/142/minor fixes

--- npc/shrink_npc.sh
+++ npc/shrink_npc.sh
@@ -2,6 +2,8 @@
 
 DEFAULT_IFS="$IFS"
 
+echo "Stage 1"
+
 cd mobs_uaro
 MOBSUARO=($(find . -type f))
 for x in "${MOBSUARO[@]}"; do
@@ -12,7 +14,10 @@ for x in "${MOBSUARO[@]}"; do
   sed -i 's/^\\.*$//' $x
   sed -i 's\^/.*$\\' $x
   sed -i '/^$/d' $x
+  echo "Done: $x"
 done
+
+echo "Stage 2"
 
 cd ../mobs
 MOBS=($(find . -type f))
@@ -24,17 +29,33 @@ for x in "${MOBS[@]}"; do
   sed -i 's/^\\.*$//' $x
   sed -i 's\^/.*$\\' $x
   sed -i '/^$/d' $x
+  echo "Done: $x"
 done
+
+echo "Stage 3"
 
 cd ..
 for x in "${MOBSUARO[@]}"; do
   while IFS=$'\t' read -r var1 var2 var3 var4; do
   IFS="," read -r v1 v2 v3 v4 v5 <<< "$var4"
-    echo -e "$var1\t$var2\t$var3\t$v1 $v2 $v3 $v4 $v5"
-    sed -i "s/^$var1\t$var2\t$var3\t$v1.*//" ./mobs_uaro/$x
+    sed -i "s/^$var1\t$var2\t.*\t$v1[,].*//" ./mobs_uaro/$x
   done < "./mobs/$x"
+  echo "Done: $x"
 done
+
+echo "Stage 4"
+
+for x in "${MOBSUARO[@]}"; do
+  while IFS= read -r var1 ; do
+    sed -i "s/^$var1$//" ./mobs_uaro/$x
+  done < "./mobs/$x"
+  echo "Done: $x"
+done
+
+
+echo "Stage 5"
 
 for x in "${MOBSUARO[@]}"; do
   sed -i '/^$/d' ./mobs_uaro/$x
+  echo "Done: $x"
 done
