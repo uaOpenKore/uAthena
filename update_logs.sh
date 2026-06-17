0193210d659bcfec533785586dd6816bd0947ce5
root <root@uaro-x64.uaro-x64>
2026-06-17 15:58:27 +0300

    update logs

--- update_logs.sh
+++ update_logs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+uts=`date +%s`
+
 cd /root/uAthena
 ./stop.sh
-tar -czf /root/uAthena/logs/log.tar.gz -C /opt/uathena/bin/log .
+tar -czf /root/uAthena/logs/log-$uts.tar.gz -C /opt/uathena/bin/log .
 git add .
 git commit -m "update logs"
 git push
