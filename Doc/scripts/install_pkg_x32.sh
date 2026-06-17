449cd769a121c372ecb9771661f372a4809266ed
fbsd61 <fbsd61@gmail.com>
2026-06-17 11:55:29 +0300

    1

--- Doc/scripts/install_pkg_x32.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt install -y libmariadb-dev:i386 libmariadb-dev-compat:i386
-
-apt install -y libmariadb3:i386 libssl-dev:i386 libssl3t64:i386 libzstd1:i386 zlib1g:i386 zlib1g-dev:i386
-
